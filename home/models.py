5c3a80ad8adcf2b9a9b4cf9dbc6066f9363d86ee
Sean Tolbert <seantolbert@Seans-MacBook-Pro.local>
2022-04-05 22:44:13 -0400

    stubs up full crud functionality

--- home/models.py
+++ home/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=20, blank=True, null=True)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from .models import Note
 
 class HomePageView(ListView):
@@ -13,7 +18,6 @@ class HomePageView(ListView):
         data = queryset[:3]
         return data
 
-
 class IndexPageView(ListView):
     template_name = 'home/index.html'
     model = Note
@@ -23,3 +27,33 @@ class IndexPageView(ListView):
 def note_detail(request, note_id):
     note = Note.objects.get(id=note_id)
     return render(request, "home/detail.html", {"note": note})
+
+class NoteCreate(CreateView, LoginRequiredMixin):
+    model = Note
+    fields = ["title", "text", "category", "tag"]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class NoteUpdate(UpdateView, LoginRequiredMixin):
+    model = Note
+    fields = ["title", "text", "category", "tag"]
+
+class QuoteDelete(DeleteView, LoginRequiredMixin):
+    model = Note
+    success_url = '/notes/'
+
+def signup(request):
+    error_message = ''
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            error_message = "Invalid signup - Please try again"
+    form = UserCreationForm()
+    context = {"form": form, "error_message": error_message}
+    return render(request, "registration/signup.html", context)

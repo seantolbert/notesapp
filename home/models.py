bd5e1898e7ec135985e50e5954623eefaf5ef195
Sean Tolbert <seantolbert@Seans-MacBook-Pro.local>
2022-04-13 20:10:22 -0400

    finished for the day

--- home/models.py
+++ home/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 
 class Category(models.Model):
     name = models.CharField(max_length=20, blank=True, null=True)
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
     label = models.CharField(max_length=20, blank=True, null=True)
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.label
@@ -20,11 +21,12 @@ class Tag(models.Model):
 class Note(models.Model):
     title = models.CharField(max_length=50, blank=True, null=True)
     date = models.DateTimeField(auto_now=True)
-    text = models.TextField()
+    # text = models.TextField()
+    text = RichTextField(blank = True, null=True)
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, null=True, related_name="notes")
     tag = models.ManyToManyField(Tag)
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
